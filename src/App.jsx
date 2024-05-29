fa78932e655822d5a704fa3d7cb7051704bc6616
Megha30501 <m_patel39@u.pacific.edu>
2024-05-29 13:31:31 -0700

    added states

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,37 @@
-function Button(props) {
-  function handleClick() {
-    console.log(` ${props.value} clicked`);
-  }
+import { useState } from "react";
+
+function Button({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={handleClick}>
-      {" "}
-      {props.value}{" "}
+    <button className="square" onClick={onSquareClick}>
+      {value}
     </button>
   );
 }
 
 const App = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
   return (
     <>
       <div className="board-row">
-        <Button value="1" />
-        <Button value="2" />
-        <Button value="3" />
+        <Button value={squares[0]} onSquareClick={handleClick} />
+        <Button value={squares[1]} onSquareClick={handleClick} />
+        <Button value={squares[2]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Button value="4" />
-        <Button value="5" />
-        <Button value="6" />
+        <Button value={squares[3]} onSquareClick={handleClick} />
+        <Button value={squares[4]} onSquareClick={handleClick} />
+        <Button value={squares[5]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Button value="7" />
-        <Button value="8" />
-        <Button value="9" />
+        <Button value={squares[6]} onSquareClick={handleClick} />
+        <Button value={squares[7]} onSquareClick={handleClick} />
+        <Button value={squares[8]} onSquareClick={handleClick} />
       </div>
     </>
   );
