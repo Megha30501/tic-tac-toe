e9c9b6fbac42a3de3eaafe5bb0ddb2ef9c291fbc
Megha30501 <m_patel39@u.pacific.edu>
2024-05-29 13:45:04 -0700

    states are working correctly

--- src/App.jsx
+++ src/App.jsx
@@ -11,27 +11,27 @@ function Button({ value, onSquareClick }) {
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
-  function handleClick() {
+  function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[0] = "X";
+    nextSquares[i] = "X";
     setSquares(nextSquares);
   }
   return (
     <>
       <div className="board-row">
-        <Button value={squares[0]} onSquareClick={handleClick} />
-        <Button value={squares[1]} onSquareClick={handleClick} />
-        <Button value={squares[2]} onSquareClick={handleClick} />
+        <Button value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Button value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Button value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Button value={squares[3]} onSquareClick={handleClick} />
-        <Button value={squares[4]} onSquareClick={handleClick} />
-        <Button value={squares[5]} onSquareClick={handleClick} />
+        <Button value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Button value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Button value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Button value={squares[6]} onSquareClick={handleClick} />
-        <Button value={squares[7]} onSquareClick={handleClick} />
-        <Button value={squares[8]} onSquareClick={handleClick} />
+        <Button value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Button value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Button value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
