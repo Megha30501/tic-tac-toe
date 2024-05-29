38b6335b610126ca40045bbf1a19e74f192ca8a3
Megha30501 <m_patel39@u.pacific.edu>
2024-05-29 12:55:25 -0700

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+function Button(props) {
+  return <button className="square"> {props.value} </button>;
+}
+
+const App = () => {
+  return (
+    <>
+      <div className="board-row">
+        <Button value="1" />
+        <Button value="2" />
+        <Button value="3" />
+      </div>
+      <div className="board-row">
+        <Button value="4" />
+        <Button value="5" />
+        <Button value="6" />
+      </div>
+      <div className="board-row">
+        <Button value="7" />
+        <Button value="8" />
+        <Button value="9" />
+      </div>
+    </>
+  );
+};
+
+export default App;
