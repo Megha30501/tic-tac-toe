58e5cee22f819629f4c910d7fdae06c13e44389d
Megha30501 <m_patel39@u.pacific.edu>
2024-05-29 18:15:33 -0700

    devided the code into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import { useState } from "react";
 import "./App.css";
-function Square({ value, onSquareClick }) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
-}
-
+import Square from "./components/Squres";
+import calculateWinner from "./components/CalculateWinner";
 const Board = ({ xIsNext, squares, onPlay }) => {
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
@@ -51,32 +45,6 @@ const Board = ({ xIsNext, squares, onPlay }) => {
   );
 };
 
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (
-      squares[a] &&
-      squares[a] === squares[b] &&
-      squares[b] === squares[c] &&
-      squares[c]
-    ) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
@@ -89,23 +57,22 @@ export default function Game() {
     setCurrentMove(nextHistory.length - 1);
   }
 
-  function jumpTo(nextMove){
+  function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-
   }
-  const moves = history.map((squares,move) =>{
+  const moves = history.map((squares, move) => {
     let description;
-    if (move > 0){
-      description = 'Go to move #' + move;
+    if (move > 0) {
+      description = "Go to move #" + move;
     } else {
-      description = 'Go to game start';
+      description = "Go to game start";
     }
     return (
       <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
-  })
+  });
   return (
     <div className="game">
       <div className="game-board">
---
+++ src/components/Squres.jsx
@@ -0,0 +1,11 @@
+import "../App.css";
+
+const Square = (props) =>{
+    return (
+        <button className="square" onClick={props.onSquareClick}>
+          {props.value}
+        </button>
+      );
+} 
+
+export default Square;
